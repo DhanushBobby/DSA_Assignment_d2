d83025230b61805844c83fb25841ae879080f05a
SHIV HARSHA VIGNESH <105575527+DhanushBobby@users.noreply.github.com>
2023-03-12 23:10:58 +0530

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,68 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
 using namespace std;
 
-#define ll long long
-#define vi(T) vector<T>
-#define pr(i,j) pair<i,j>
-#define all(vec) vec.begin(), vec.end()
-#define pb push_back
-#define ppb pop_back
-#define ff first
-#define ss second
-#define f(i,A,B) for(long long i=A;i<B;i++)
-#define M 1000000007
+unordered_map<char, string> keypad = {
+    {'A', "2"}, {'B', "2"}, {'C', "2"},
+    {'D', "3"}, {'E', "3"}, {'F', "3"},
+    {'G', "4"}, {'H', "4"}, {'I', "4"},
+    {'J', "5"}, {'K', "5"}, {'L', "5"},
+    {'M', "6"}, {'N', "6"}, {'O', "6"},
+    {'P', "7"}, {'Q', "7"}, {'R', "7"}, {'S', "7"},
+    {'T', "8"}, {'U', "8"}, {'V', "8"},
+    {'W', "9"}, {'X', "9"}, {'Y', "9"}, {'Z', "9"}
+};
 
-// output
-#define input(vec) for(auto &i:vec)cin>>i
-#define output(vec) for(auto &i:vec){cout<<i<<" ";}cout<<"\n"
-#define outputpair1(vec) for(auto &i:vec){cout<<"{"<<i.first<<" "<<i.second<<"} ";}cout<<"\n"
-#define outputpair2(vec) for(auto &i:vec){cout<<i.first<<" "<<i.second<<"\n";}
-#define google(i) cout<<"Case #"<<i<<": "
-
-// Function
-ll gcd(ll a, ll b) {if (a == 0) {return b;} return gcd(b % a, a);}
-ll power(ll a, ll b) {
-    //a ki power b
-    ll ans = 1; ll mul = a;
-    while (b > 0) {if (b & 1) {ans *= mul;} mul = mul * mul; b >>= 1;}
-    return ans;
-}
-unsigned ll SetBits(unsigned ll n) {
-    unsigned ll count = 0;
-    while (n) {count += n & 1; n >>= 1;}
-    return count;
-}
-
-// Code start
-const ll N = 1e5;
-ll n, m, k, num;
-ll a, b, c, d;
-string s;
-
-void solve(ll t) {
-    cin >> s;
-    vi(string) vec = { "2",   "22",  "222", "3",   "33",   "333", "4",
-                       "44",  "444", "5",   "55",  "555",  "6",   "66",
-                       "666", "7",   "77",  "777", "7777", "8",   "88",
-                       "888", "9",   "99",  "999", "9999"
-                     };
-    f(i, 0, s.size()) {
-        cout << vec[s[i]-'A'];
+string toKeypadSequence(string sentence) {
+    string keypadSequence;
+    for (char c : sentence) {
+        if (isalpha(c)) {
+            keypadSequence += keypad[toupper(c)];
+        }
     }
-    cout << "\n";
+    return keypadSequence;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    // your code goes here
-    ll t = 1;
-    cin >> t;
-    // while(t--)solve();
-    ll i = 0;
-    f(i, 0, t) {
-        solve(i + 1);
-    }
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+    string keypadSequence = toKeypadSequence(sentence);
+    cout << "The keypad sequence is: " << keypadSequence << endl;
     return 0;
 }
