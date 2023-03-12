6d7c3749388d2e8db34328133e80194a23314b67
SHIV HARSHA VIGNESH <105575527+DhanushBobby@users.noreply.github.com>
2023-03-12 23:15:49 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,72 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-#define ll long long 
-#define vi(T) vector<T>  
-#define pr(i,j) pair<i,j>
-#define all(vec) vec.begin(), vec.end() 
-#define pb push_back
-#define ppb pop_back
-#define ff first
-#define ss second
-#define f(i,A,B) for(long long i=A;i<B;i++)
-#define M 1000000007
-
-// output
-#define input(vec) for(auto &i:vec)cin>>i
-#define output(vec) for(auto &i:vec){cout<<i<<" ";}cout<<"\n"
-#define outputpair1(vec) for(auto &i:vec){cout<<"{"<<i.first<<" "<<i.second<<"} ";}cout<<"\n"
-#define outputpair2(vec) for(auto &i:vec){cout<<i.first<<" "<<i.second<<"\n";}
-#define google(i) cout<<"Case #"<<i<<": "
-
-// Function
-ll gcd(ll a, ll b){if (a == 0){return b;} return gcd(b % a, a);}
-ll power(ll a,ll b){
-    //a ki power b
-    ll ans=1;ll mul=a;
-    while(b>0){if(b&1){ans *= mul;}mul = mul*mul;b>>=1;}
-    return ans;
-}
-unsigned ll SetBits(unsigned ll n){
-    unsigned ll count = 0;
-    while (n) {count += n & 1;n >>= 1;}
-    return count;
+// Original function
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
 
-// Code start 
-const ll N = 1e5;
-ll n,m,k,num;
-ll a,b,c,d;
-string s;
-
-
-void fact(ll n, ll k){
-    if(n==0){
-        cout<<k<<"\n";
-        return ;
-    }
-    k *= n;
-    fact(n-1,k);
+// Tail recursive function
+int tailFactorial(int n, int accumulator = 1) {
+    if (n == 0)
+        return accumulator;
+    else
+        return tailFactorial(n - 1, n * accumulator);
 }
 
-
-void solve(ll t){
-    cin>>n;
-    ll cnt=1;
-    fact(n,cnt);
-}       
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    // your code goes here
-    ll t=1;
-    cin>>t;
-    // while(t--)solve();
-    ll i=0;
-    f(i,0,t){
-        solve(i+1);
-    }
+int main() {
+    cout << "Factorial of 5: " << factorial(5) << endl;
+    cout << "Tail Recursive Factorial of 5: " << tailFactorial(5) << endl;
     return 0;
 }
